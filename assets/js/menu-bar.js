c5d918d680402e2b4654b0967114e00d1a59f5e4
juandbravo7320 <juandiego732000@gmail.com>
2022-03-18 14:09:26 -0500

    Falta arreglar el menú desplegable

---
+++ assets/js/menu-bar.js
@@ -0,0 +1,23 @@
+const hamburger = document.querySelector('.header__button')
+const sidebar = document.querySelector('.sidebar')
+
+const sidebarButton= document.querySelectorAll('.sidebar__a')
+
+hamburger.addEventListener('click', () => {
+    
+    if (sidebar.classList.contains('active')) {
+        
+        return sidebar.classList.remove('active')
+    }else{
+        
+        return sidebar.classList.add('active')
+    }
+})
+
+sidebarButton.forEach(button => {
+    button.addEventListener('click', () => {
+        return sidebar.classList.remove('active')
+    })
+}
+)
+
