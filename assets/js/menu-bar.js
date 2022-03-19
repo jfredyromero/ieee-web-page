d7398a9d666e42948db3961faaddfb6696632f30
Jhon Fredy Romero <jhonrom@unicauca.edu.co>
2022-03-19 18:55:48 -0500

    Dropdown menu added. Style details and animations left

--- assets/js/menu-bar.js
+++ assets/js/menu-bar.js
@@ -1,21 +1,22 @@
-const hamburger = document.querySelector('.header__button')
-const sidebar = document.querySelector('.sidebar')
+const hamburger = document.querySelector(".header__button");
+const sidebar = document.querySelector(".sidebar");
+const sidebarButton = document.querySelectorAll(".sidebar__a");
+const dropdownButton = document.querySelector("#dropdown");
 
-const sidebarButton= document.querySelectorAll('.sidebar__a')
+hamburger.addEventListener("click", () => {
+	if (sidebar.classList.contains("active")) {
+		return sidebar.classList.remove("active");
+	} else {
+		return sidebar.classList.add("active");
+	}
+});
 
-hamburger.addEventListener('click', () => {
-    
-    if (sidebar.classList.contains('active')) {
-        return sidebar.classList.remove('active')
-    }else{
-        return sidebar.classList.add('active')
-    }
-})
+sidebarButton.forEach((button) => {
+	button.addEventListener("click", () => {
+		return sidebar.classList.remove("active");
+	});
+});
 
-sidebarButton.forEach(button => {
-    button.addEventListener('click', () => {
-        return sidebar.classList.remove('active')
-    })
-}
-)
-
+dropdownButton.addEventListener("click", () => {
+	document.querySelector(".header__dropdown").classList.toggle("active");
+});
