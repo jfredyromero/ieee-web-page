4336011210a1a84913adc1ac940557789762c81f
juandbravo7320 <juandiego732000@gmail.com>
2022-05-14 12:10:09 -0500

    Query getJuntaDirectiva añadido

--- models/OrganizationModel.php
+++ models/OrganizationModel.php
@@ -12,11 +12,19 @@ class OrganizationModel{
     
     public function getJuntaDirectiva(){
         $this->db->getConnection();
-        $query = "SELECT * FROM miembros";
+        $query = "SELECT * FROM miembros AS m INNER JOIN cargos_de_miembros AS cdm ON m.id = cdm.miembro INNER JOIN cargos AS c ON cdm.cargo = c.id WHERE (c.id BETWEEN 4 AND 8) AND (m.anioSalidaRama IS NULL)";
         $result = $this->db->getData($query);
         $this->db->closeConnection();
         return $result;
-    }   
+    }
+
+    public function getComites(){
+        $this->db->getConnection();
+        $query = "SELECT * FROM miembros AS m INNER JOIN cargos_de_miembros AS cdm ON m.id = cdm.miembro INNER JOIN cargos AS c ON cdm.cargo = c.id WHERE (c.id BETWEEN 4 AND 8) AND (m.anioSalidaRama IS NULL)";
+        $result = $this->db->getData($query);
+        $this->db->closeConnection();
+        return $result;
+    }
 }
 
 ?>
