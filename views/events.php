af0d00a4db3d3766b465ee801e0e662490afed90
JorgeVargas <javargas216@unicauca.edu.co>
2022-07-09 11:11:17 -0500

    estilos nuevos seccion eventos Co-authored-by: SantiagoYangana <SantiagoYangana@users.noreply.github.com>

--- views/events.php
+++ views/events.php
@@ -54,18 +54,36 @@
 
     <section class="cd-timeline js-cd-timeline">
         <div class="container max-width-lg cd-timeline__container">
+
+            <div class="cd-timeline__block">
+                <div class="cd-timeline__img cd-timeline__img--movie">
+                    <img src="assets/img/cd-icon-movie.svg" alt="Movie">
+                </div> <!-- cd-timeline__img -->
+
+                <div class="cd-timeline__content text-component" id="tet2022">
+                    <img class="img_event" src="assets/img/header.png" alt="">
+                    <p class="p_event">TET 2022</p>
+                    <!-- <h2>TET 2022</h2>
+                    <p class="color-contrast-medium">This is the content of the last section</p> -->
+
+                    <div class="flex justify-between items-center">
+                        <span class="cd-timeline__date">Feb 26</span>
+                    </div>
+                </div>
+            </div> <!-- cd-timeline__block -->
+
+
             <div class="cd-timeline__block">
                 <div class="cd-timeline__img cd-timeline__img--picture">
                     <img src="assets/img/cd-icon-picture.svg" alt="Picture">
                 </div> <!-- cd-timeline__img -->
 
                 <div class="cd-timeline__content text-component">
-                    <h2>IEEE Day</h2>
-                    <p class="color-contrast-medium">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.</p>
+                    <img class="img_event" src="assets/img/header.png" alt="">
+                    <p class="p_event">IEEE Day</p>
 
                     <div class="flex justify-between items-center">
                         <span class="cd-timeline__date">Jan 14</span>
-                        <a href="#0" id="ieeeday" class="btn btn--subtle">Mirar Foto</a>
                     </div>
                 </div> <!-- cd-timeline__content -->
             </div> <!-- cd-timeline__block -->
@@ -76,12 +94,11 @@
                 </div> <!-- cd-timeline__img -->
 
                 <div class="cd-timeline__content text-component">
-                    <h2>Navidad FIEEELIZ</h2>
-                    <p class="color-contrast-medium">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde?</p>
+                    <img class="img_event" src="assets/img/header.png" alt="">
+                    <p class="p_event">Navidad FIEEELIZ</p>
 
                     <div class="flex justify-between items-center">
                         <span class="cd-timeline__date">Jan 18</span>
-                        <a href="#0" class="btn btn--subtle">Mirar Foto</a>
                     </div>
                 </div> <!-- cd-timeline__content -->
             </div> <!-- cd-timeline__block -->
@@ -92,12 +109,11 @@
                 </div> <!-- cd-timeline__img -->
 
                 <div class="cd-timeline__content text-component">
-                    <h2>Día de la mujer</h2>
-                    <p class="color-contrast-medium">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, obcaecati, quisquam id molestias eaque asperiores voluptatibus cupiditate error assumenda delectus odit similique earum voluptatem doloremque dolorem ipsam quae rerum quis. Odit, itaque, deserunt corporis vero ipsum nisi eius odio natus ullam provident pariatur temporibus quia eos repellat consequuntur perferendis enim amet quae quasi repudiandae sed quod veniam dolore possimus rem voluptatum eveniet eligendi quis fugiat aliquam sunt similique aut adipisci.</p>
+                    <img class="img_event" src="assets/img/header.png" alt="">
+                    <p class="p_event">Día de la Mujer</p>
 
                     <div class="flex justify-between items-center">
                         <span class="cd-timeline__date">Jan 24</span>
-                        <a href="#0" class="btn btn--subtle">Mirar Foto</a>
                     </div>
                 </div> <!-- cd-timeline__content -->
             </div> <!-- cd-timeline__block -->
@@ -108,12 +124,11 @@
                 </div> <!-- cd-timeline__img -->
 
                 <div class="cd-timeline__content text-component">
-                    <h2>Día del niño</h2>
-                    <p class="color-contrast-medium">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.</p>
+                    <img class="img_event" src="assets/img/header.png" alt="">
+                    <p class="p_event">Día del Niño</p>
 
                     <div class="flex justify-between items-center">
                         <span class="cd-timeline__date">Feb 14</span>
-                        <a href="#0" class="btn btn--subtle">Mirar Foto</a>
                     </div>
                 </div> <!-- cd-timeline__content -->
             </div> <!-- cd-timeline__block -->
@@ -124,12 +139,11 @@
                 </div> <!-- cd-timeline__img -->
 
                 <div class="cd-timeline__content text-component">
-                    <h2>Día de la madre</h2>
-                    <p class="color-contrast-medium">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum.</p>
+                    <img class="img_event" src="assets/img/header.png" alt="">
+                    <p class="p_event">Día de la Madre</p>
 
                     <div class="flex justify-between items-center">
                         <span class="cd-timeline__date">Feb 18</span>
-                        <a href="#0" class="btn btn--subtle">Mirar Foto</a>
                     </div>
                 </div> <!-- cd-timeline__content -->
             </div> <!-- cd-timeline__block -->
@@ -140,8 +154,8 @@
                 </div> <!-- cd-timeline__img -->
 
                 <div class="cd-timeline__content text-component">
-                    <h2>BIEEEnvenida de Primiparos</h2>
-                    <p class="color-contrast-medium">This is the content of the last section</p>
+                    <img class="img_event" src="assets/img/header.png" alt="">
+                    <p class="p_event">BIEEENVENIDA de Primiparos</p>
 
                     <div class="flex justify-between items-center">
                         <span class="cd-timeline__date">Feb 26</span>
@@ -155,8 +169,8 @@
                 </div> <!-- cd-timeline__img -->
 
                 <div class="cd-timeline__content text-component">
-                    <h2>Ramakon</h2>
-                    <p class="color-contrast-medium">This is the content of the last section</p>
+                    <img class="img_event" src="assets/img/header.png" alt="">
+                    <p class="p_event">RAMAKON</p>
 
                     <div class="flex justify-between items-center">
                         <span class="cd-timeline__date">Feb 26</span>
@@ -164,20 +178,7 @@
                 </div> <!-- cd-timeline__content -->
             </div> <!-- cd-timeline__block -->
 
-            <div class="cd-timeline__block">
-                <div class="cd-timeline__img cd-timeline__img--movie">
-                    <img src="assets/img/cd-icon-movie.svg" alt="Movie">
-                </div> <!-- cd-timeline__img -->
 
-                <div class="cd-timeline__content text-component">
-                    <h2>TET 2022</h2>
-                    <p class="color-contrast-medium">This is the content of the last section</p>
-
-                    <div class="flex justify-between items-center">
-                        <span class="cd-timeline__date">Feb 26</span>
-                    </div>
-                </div> <!-- cd-timeline__content -->
-            </div> <!-- cd-timeline__block -->
 
         </div>
     </section> <!-- cd-timeline -->
@@ -207,6 +208,19 @@
                 showConfirmButton: false,
             })
         })
+
+        document.getElementById("tet2022").addEventListener("click", () => {
+            Swal.fire({
+                title: "TET 2022",
+                width: 1200,
+                imageUrl: './assets/img/header.png',
+                imageWidth: 1200,
+                imageHeight: 500,
+                imageAlt: 'Custom image',
+                showConfirmButton: false,
+            })
+        })
+    
     </script>
 
     <?php
