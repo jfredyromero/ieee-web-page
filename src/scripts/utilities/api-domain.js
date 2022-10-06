8e01d41316446d4ab56c7506741d0877b3a5193a
JorgeVargas <javargas216@unicauca.edu.co>
2022-10-06 11:47:12 -0500

    Changes for production api domain

--- src/scripts/utilities/api-domain.js
+++ src/scripts/utilities/api-domain.js
@@ -1,5 +1,5 @@
 const API_DOMAIN =
-	(import.meta.env.VITE_PROD === "true"
+	(import.meta.env.VITE_PROD == "true"
 		? "http://ieee.unicauca.edu.co" +import.meta.env.VITE_PROD_DOMAIN
 		: "http://localhost" + import.meta.env.VITE_DEV_DOMAIN) +
 	"api/";
